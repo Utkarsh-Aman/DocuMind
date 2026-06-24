1b94b800afbcd8210e47d69867228338f94b7ddc
Utkarsh-Aman <utkarsh4aman@gmail.com>
2026-06-25 04:06:32 +0530

    fix: updated CORS and COOP settings for frontend domains

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://documind-lyart.vercel.app"
+        "https://documind-lyart.vercel.app",
+        "https://documind-v2.netlify.app",
+        "https://documind-v1.netlify.app"
     ],
     allow_origin_regex=r"https?://(localhost|127\.0\.0\.1|10\.\d+\.\d+\.\d+|192\.168\.\d+\.\d+|172\.(1[6-9]|2\d|3[0-1])\.\d+\.\d+)(:\d+)?",
     allow_credentials=True,
